d5278d7c01020c7c51753f11f8a07262c55ab046
Advait Risbud <advaitris@example.com>
2026-05-06 15:03:51 -0400

    Added input field and add habit button to Habitform component

--- src/components/Habitform.tsx
+++ src/components/Habitform.tsx
@@ -1,8 +1,8 @@
 import { Button } from "./Button"
 
 export function Habitform(){
-    return <form>
-        <input/>
+    return <form className="flex gap-2">
+        <input className="flex-1 rounded-lg bg-zinc-800 px-4 py-2 outline-none focus-visible:ring-2 focus-visible:ring-violet-500"/>
         <Button>Add Habit</Button>
     </form>;
 }
