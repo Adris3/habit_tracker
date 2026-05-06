32c1381a6554252dfa077615fbee2756d682451f
Advait Risbud <advaitris@example.com>
2026-05-06 15:56:12 -0400

    Adjusted padding on habit list

--- src/components/Habitlist.tsx
+++ src/components/Habitlist.tsx
@@ -23,7 +23,7 @@ type HabititemProps = {
 
 function Habititem({ habit }: HabititemProps) {
     // return <h1>{habit.name}</h1>
-    return <div className="rounded-xl bg-zinc-800 p-4">
+    return <div className="rounded-xl bg-zinc-800 p-4 flex-col gap-3">
         <div className="flex items-center justify-between mb-3">{habit.name}</div>
     </div>
 }
