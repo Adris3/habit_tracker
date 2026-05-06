251affe35bdf7313b24e3b06f95cfb45642c6e89
Advait Risbud <advaitris@example.com>
2026-05-06 14:33:28 -0400

    Turned Header into a self closing element

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from "./components/Button"
 export default function App() {
   return (
   <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
-    <Header></Header>
+    <Header/>
   </div>
   )
 }
