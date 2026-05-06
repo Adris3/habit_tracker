c5cb84c91656d74edc63c40de21d0655a09ba903
Advait Risbud <advaitris@example.com>
2026-05-06 10:39:08 -0400

    Added header using function

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
 export default function App() {
-  return <h1 className = "text-red-500">This is an html header</h1>;
+  return (
+  <div>
+    <Header></Header>
+  </div>
+  )
+}
+
+function Header(){
+  return <div></div>
 }
