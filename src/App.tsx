f2bf2f8ef980ca30778250ca8abb01d1cd8b43be
Advait Risbud <advaitris@example.com>
2026-05-06 14:34:56 -0400

    Added a Habitform componenet

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Button } from "./components/Button"
+import { Habitform } from "./components/Habitform"
 
 export default function App() {
   return (
   <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
     <Header/>
+    <Habitform/>
   </div>
   )
 }
