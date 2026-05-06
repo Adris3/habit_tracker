61c39a906b469e46d26df64b5c14875e790fa9b8
Advait Risbud <advaitris@example.com>
2026-05-06 15:20:42 -0400

    Rendering onto the page from the habits list

--- src/components/Habitlist.tsx
+++ src/components/Habitlist.tsx
@@ -1,11 +1,18 @@
 import { Button } from "./Button";
 
 export function Habitlist() {
-    const habits = [];
+    const habits = [{id: 1, name: "hi"},
+                    {id: 2, name: "hello"},
+                    {id: 3, name: "greetings"},
+    ];
 
     if (habits.length === 0) {
         return <p className="text-center text-zinc-500 py-12">No habits yet. Add one above to get started.</p>
     }
 
-    return <h1>Full</h1>;
+    return <div className="flex flex-col gap-3">
+        {habits.map(habit => (
+            <h1>{habit.name}</h1>
+        ))}
+    </div>;
 }
