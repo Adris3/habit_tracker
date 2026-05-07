2ccb25798a0a12dc84aa291fbe5b614d89b9b309
Advait Risbud <advaitris@example.com>
2026-05-07 12:47:44 -0400

    Finished up styling of buttons

--- src/components/Habitlist.tsx
+++ src/components/Habitlist.tsx
@@ -33,12 +33,13 @@ function Habititem({ habit }: HabititemProps) {
                     <span className="text-sm text-amber-400">3</span>
                 </div>
 
-                <Button variant="ghost-destructive">Delete</Button>
+                <Button variant="ghost-destructive" className="text-sm">Delete</Button>
             </div>
 
             <div className="flex gap-1.5">
                 {visibleDates.map(date => (
-                    <Button className= "flex flex-1 flex-col"key={date.toISOString()} disabled={isFuture(date)}>
+                    <Button className= "flex flex-1 flex-col items-center gap-0.5 rounded-lg text-xs"
+                        key={date.toISOString()} disabled={isFuture(date)}>
                         <span className="font-medium">{format(date, "EEE")}</span>
                         <span>{format(date, "d")}</span>
                     </Button>
