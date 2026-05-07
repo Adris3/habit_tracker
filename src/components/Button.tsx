57f3e48c06b056b576c28bbff89786e9aa62b8b8
Advait Risbud <advaitris@example.com>
2026-05-07 12:15:37 -0400

    Used {...props} instead of manually typing each prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,13 @@
-import type { ReactNode } from "react"
+import type { ComponentProps, ReactNode } from "react"
 
 type ButtonProps = {
     children: ReactNode
-    disabled?: boolean
-}
-
-export function Button({children, disabled = false}: ButtonProps) {
+} & ComponentProps<"button"> 
+ 
+export function Button({...props}: ButtonProps) {
     
-    return <button disabled = {disabled} className="bg-violet-600 hover:bg-violet-500 
+    return <button 
+    {...props} className="bg-violet-600 hover:bg-violet-500 
     transition-colors rounded px-2 py-1 
-    disabled:opacity-30 disabled:cursor-not-allowed">{children}</button>
+    disabled:opacity-30 disabled:cursor-not-allowed"/>
 }
