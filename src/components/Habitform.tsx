a744f9c173dd55280eb80944ef1d32ce60c30f31
Advait Risbud <advaitris@example.com>
2026-05-07 12:55:27 -0400

    Disabled add button if input field is empty

--- src/components/Habitform.tsx
+++ src/components/Habitform.tsx
@@ -1,9 +1,21 @@
+import { useState, type SubmitEvent } from "react";
 import { Button } from "./Button"
 
 export function Habitform(){
-    return <form className="flex gap-2">
-        <input className="flex-1 rounded-lg bg-zinc-800 px-4 py-2 outline-none focus-visible:ring-2 focus-visible:ring-violet-500"
+    const [name, setName] = useState("")
+
+    function handleSubmit(e: SubmitEvent){
+        e.preventDefault()
+    }
+
+    return <form className="flex gap-2" onSubmit={handleSubmit}>
+        <input 
+        value= {name}
+        onChange={e => setName(e.target.value)}
+        className="flex-1 rounded-lg bg-zinc-800 px-4 py-2 outline-none focus-visible:ring-2 focus-visible:ring-violet-500"
         placeholder="New Habit..."/>
-        <Button className="rounded-lg px-4 font-medium">Add Habit</Button>
+        <Button 
+        disabled={name.trim() === ""}
+        className="rounded-lg px-4 font-medium">Add Habit</Button>
     </form>;
 }
