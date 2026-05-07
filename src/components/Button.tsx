895aad30e73bff0bec8585c1ae8906dc97a25899
Advait Risbud <advaitris@example.com>
2026-05-07 12:30:05 -0400

    Added primary, secondary, and ghost-destructive styles to buttons.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 import type { ComponentProps } from "react";
 
-type Variant = "primary" | "secondary" | "ghost destructive";
+type Variant = "primary" | "secondary" | "ghost-destructive";
 
 type ButtonProps = {
     variant?: Variant;
@@ -9,9 +9,20 @@ type ButtonProps = {
 export function Button({variant = "primary", ...props}: ButtonProps) {
     
     return <button 
-    {...props} className="bg-violet-600 hover:bg-violet-500 
-    transition-colors rounded px-2 py-1 
-    disabled:opacity-30 disabled:cursor-not-allowed"/>
+    {...props} className={
+    `${getVariantStyles(variant)} transition-colors rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed`}
+    />
 }
 
-function getVariantStyles(variant: Variant) {}
+function getVariantStyles(variant: Variant) {
+    switch(variant) {
+        case "primary":
+            return "bg-violet-600 hover:bg-violet-500 "
+        case "secondary":
+            return "bg-zinc-700 hover:bg-zinc-600 text-zinc-400"
+        case "ghost-destructive":
+            return "hover:bg-red-900 text-red-800 hover:text-red-200"
+        default:
+            throw new Error(`Invalid variants: ${variant satisfies never}`)
+    }
+}
