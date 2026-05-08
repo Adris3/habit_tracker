e471a1cd3d942deb5a9e957f5f6347a5dc51aad3
Advait Risbud <advaitris@example.com>
2026-05-08 14:46:20 -0400

    Added comment in getStreak() function

--- src/components/Habitlist.tsx
+++ src/components/Habitlist.tsx
@@ -65,14 +65,6 @@ function Habititem({ habit, deleteHabit, toggleHabit}: HabititemProps) {
 }
 
 function getStreak(completions: Date[]) {
-    // let streak = 0;
-    // let date = new Date();
-
-    // while(completions.some(c => isSameDay(c, date))){
-    //     streak++;
-    //     date = subDays(date, 1);
-    // }
-
-    // return streak
+    // Simply returns length of completions array instead of overcomplicated date calculations
     return completions.length
 }
