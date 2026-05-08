4998596172441a04a32c2a429e92b44ef6fcb26b
Advait Risbud <advaitris@example.com>
2026-05-08 10:08:33 -0400

    Added deleteHabit function

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,11 @@ export default function App() {
     setHabits(curr => [...curr, {id: crypto.randomUUID(), name}])
   }
 
+  function deleteHabit(id: String) {
+    setHabits(curr => curr.filter(h => h.id !== id))
+  }
+
+
   return (
   <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
     <Header/>
